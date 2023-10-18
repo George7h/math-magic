41c2685217c1b80e93a03199af27828dd47be803
George <georgie.george7@gmail.com>
2023-10-18 11:31:50 +0400

    Import Calculator in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Calculator from './components/calculator';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <Calculator />
     </div>
   );
 }
